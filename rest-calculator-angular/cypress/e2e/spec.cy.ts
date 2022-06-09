1757ea688d6f5b3c1f9a420503635950a68335ab
Anel <meirzhananel@gmail.com>
2022-06-09 14:06:21 +0200

    Button test (click)

---
+++ rest-calculator-angular/cypress/e2e/spec.cy.ts
@@ -0,0 +1,11 @@
+describe('buttons test', () => {
+  it('passes', () => {
+  cy.visit('http://localhost:4200')
+  cy.get('button').contains('1').click()
+  cy.get('button').contains('+').click()
+  cy.get('button').contains('1').click()
+  cy.get('button').contains('=').click()
+  cy.get('input').should('2')
+
+  })
+})
